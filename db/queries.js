9ffda7a94a07ebd3d687a113224cec5c119fcabe
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-20 00:16:00 -0700

    Changed query format for getallemployees and getemployeesbymanager.

--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,16 @@
 // SQL query commands for export
 
 // Employee Queries
-const getAllEmployees = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS Title, d.name AS Department, r.salary AS Salary, CONCAT(m.first_name, ', ', m.last_name) AS Manager
+const getAllEmployees = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS Title, d.name AS Department, CONCAT('$', r.salary) AS Salary, CONCAT(m.first_name, ', ', m.last_name) AS Manager
 FROM employee AS e
 INNER JOIN role AS r ON e.role_id = r.id
 LEFT JOIN employee AS m ON e.manager_id = m.id
 INNER JOIN department AS d ON  r.department_id = d.id;`;
 
-const getEmployeesByManager = `SELECT  CONCAT(m.first_name, ', ', m.last_name) AS Manager, GROUP_CONCAT(CONCAT(e.first_name, ' ', e.last_name) SEPARATOR ', ') AS 'Employees' 
+const getEmployeesByManager = `SELECT m.id AS 'Manager ID', CONCAT(m.first_name, ', ', m.last_name) AS Manager, e.id AS 'Employee ID', CONCAT(e.first_name, ', ', e.last_name) AS 'Employee'
 FROM employee AS e
-INNER JOIN employee AS m ON e.manager_id = m.id
-GROUP BY Manager;`
+JOIN employee AS m ON e.manager_id = m.id
+ORDER BY Manager;`
 
 const getEmployeesByDepartment = `SELECT  d.name AS department, e.id AS 'Employee ID', e.first_name AS 'First Name', e.last_name AS 'Last Name'
 FROM employee AS e
@@ -33,7 +33,7 @@ WHERE id = ?;`;
 
 
 // Role queries
-const getAllRoles = `SELECT r.title AS Title, r.id AS 'Role ID', d.name AS Department, CONCAT('$', r.salary) AS Salary
+const getAllRoles = `SELECT r.id AS 'Role ID', r.title AS Title, d.name AS Department, CONCAT('$', r.salary) AS Salary
 FROM role AS r
 LEFT JOIN department AS d ON r.department_id = d.id;`;
 
