ed95a5a2efa715c693a0189c6075888835d09236
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-20 00:02:47 -0700

    Employees grouped by manager presents a list of employees under each manager.

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ INNER JOIN role AS r ON e.role_id = r.id
 LEFT JOIN employee AS m ON e.manager_id = m.id
 INNER JOIN department AS d ON  r.department_id = d.id;`;
 
-const getEmployeesByManager = `SELECT CONCAT(m.first_name, ', ', m.last_name) AS Manager, e.id AS 'Employee ID', e.first_name AS 'First Name', e.last_name AS 'Last Name'
+const getEmployeesByManager = `SELECT  CONCAT(m.first_name, ', ', m.last_name) AS Manager, GROUP_CONCAT(CONCAT(e.first_name, ' ', e.last_name) SEPARATOR ', ') AS 'Employees' 
 FROM employee AS e
 INNER JOIN employee AS m ON e.manager_id = m.id
 GROUP BY Manager;`
